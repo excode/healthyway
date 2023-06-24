0ea579e53abf2c681e4f15b389d4d30b3fb33042
Sakib Rahman <seu.sakib@gmail.com>
2023-06-24 12:05:14 +0600

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1c9887ff1888f95"; }
+/******/ 		__webpack_require__.h = function() { return "a73fb53aca2889ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
